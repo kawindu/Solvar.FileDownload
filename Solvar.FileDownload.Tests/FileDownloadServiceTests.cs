54849362b786b6972497f0561a9c63eb4b6e9ae5
kawindu <32182772+kawindu@users.noreply.github.com>
2023-07-13 16:24:48 +1200

    Adding unit tests

---
+++ Solvar.FileDownload.Tests/FileDownloadServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using Moq;
+using Moq.Protected;
+
+namespace Solvar.FileDownload.Tests
+{
+    [TestFixture]
+    public class FileDownloadServiceTests
+    {
+        [Test]
+        public async Task DownloadFilesAsync_SuccessfullyDownloadsFiles()
+        {
+            // Arrange
+            var url = "http://example.com/file2.txt";
+
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new ByteArrayContent(new byte[0])
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var fileDownloader = new FileDownloadService();
+
+            // Act
+            await fileDownloader.DownloadFileAsync(url, httpClient);
+
+            // Assert
+            var fileName = Path.GetFileName(url);
+            mockHttpMessageHandler.Protected().Verify("SendAsync", Times.Once(), ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
+            Assert.IsTrue(File.Exists(fileName));
+            File.Delete(fileName);
+        }
+    }
+}
